1f0a78d87808c3bdabd882bb09936dfc008217c9
Hector Hurtarte <hectorh30@gmail.com>
2018-04-27 19:56:38 -0600

    Data update

--- config/environment.js
+++ config/environment.js
@@ -29,7 +29,7 @@ module.exports = function(environment) {
       // Set null to retrieve data from the spreadsheet live. Otherwhise set the
       // URL from which to load de dumped static files
       staticFilesUrl: null
-      // staticFilesUrl: 'http://192.168.250.206:6360/static-files/'
+      // staticFilesUrl: 'http://db.devservir6:6360/static-files/'
       // staticFilesUrl: 'http://eleccionpdh.org/static-files/'
     },
 
@@ -47,9 +47,9 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
     ENV.contentSecurityPolicy = contentSecurityPolicy;
-    ENV.contentSecurityPolicy['script-src'] = "'self' 'unsafe-eval' 192.168.250.206:* 172.20.10.9:*";
+    ENV.contentSecurityPolicy['script-src'] = "'self' 'unsafe-eval' db.devservir6:* 172.20.10.9:*";
 
-    ENV.APP.staticFilesUrl = 'http://192.168.250.206:6360/static-files/';
+    ENV.APP.staticFilesUrl = 'http://db.devservir6:6360/static-files/';
   }
 
   if (environment === 'test') {
